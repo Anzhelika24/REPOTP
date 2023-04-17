b14496826f184840920e60554b1a7819b77afbeb
Anzhelika24 <114088947+Anzhelika24@users.noreply.github.com>
2023-04-17 04:38:32 +0300

    Update install.sh

--- python_proj1/install.sh
+++ python_proj1/install.sh
@@ -3,11 +3,8 @@
 git clone -b main git@github.com:Anzhelika24/REPOTP.git
 
 # shellcheck disable=SC2164
-cd Python_projects/python_proj
+cd REPOTP/python_proj
 
 pip install -r requirements.txt
 
 chmod +x main.py
-
-
-
